6d86501606fb87b12597443aa2dc5a64aa51ec8c
alberto <alberto@llamocca.com>
2024-05-27 20:20:26 +0000

    configure clang

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -5,6 +5,7 @@
   channel = "stable-23.11"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
+    pkgs.clang_17
     # pkgs.go
     # pkgs.python311
     # pkgs.python311Packages.pip
